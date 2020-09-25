7d7a9d1e6d92d67ab5ffffdc704ab5c1e1a710bf
Prithvipatel007 <prithvipatel007@gmail.com>
2020-09-25 18:29:57 +0200

    character animation and left and right movement added

---
+++ ZombieShooterPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    ///  movement Variables
+    /// </summary>
+    private float runSpeed = 8f;
+    Rigidbody myRB;
+    Animator myAnim;
+    bool facingRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.z *= -1;
+        transform.localScale = theScale;
+    }
+
+}
