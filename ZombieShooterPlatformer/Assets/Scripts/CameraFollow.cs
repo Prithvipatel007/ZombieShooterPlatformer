5860e131682c419e2df55c7a3020669c9b8bcbf2
Prithvipatel007 <prithvipatel007@gmail.com>
2020-09-26 02:20:45 +0200

    camera's dynamic motion implemented

---
+++ ZombieShooterPlatformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;        // to whom the camera follows
+    public float smoothing = 5f;    // smoothness
+
+    Vector3 offset;                 //  distance it maintains with the target when following
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;      // camera's position - target's position
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
