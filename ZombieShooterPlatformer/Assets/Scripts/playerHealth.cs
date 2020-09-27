4ec33da8e98801cc475d0936fe6fa90ddc5c47ff
Prithvipatel007 <prithvipatel007@gmail.com>
2020-09-27 13:11:27 +0200

    health code with slider implemented and damage screen implemented

--- ZombieShooterPlatformer/Assets/Scripts/playerHealth.cs
+++ ZombieShooterPlatformer/Assets/Scripts/playerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerHealth : MonoBehaviour
 {
@@ -9,21 +10,41 @@ public class playerHealth : MonoBehaviour
 
     public GameObject playerDeathFX;
 
+    // HUD
+    public Slider playerHealthSlider;
+    public Image damageScreen;
+    Color flashColor = new Color(255f, 255f, 255f, 1);
+    float flashSpeed = 5f;
+    bool damaged = false;       // indicate if the player is damaged or not.
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = fullHealth;
+        playerHealthSlider.maxValue = fullHealth;
+        playerHealthSlider.value = currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // are we hurt?
+        if (damaged)
+        {
+            damageScreen.color = flashColor;
+        }
+        else
+        {
+            damageScreen.color = Color.Lerp(damageScreen.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
     }
 
     public void addDamage(float damage)
     {
         currentHealth -= damage;
+        playerHealthSlider.value = currentHealth;
+        damaged = true;
 
         if(currentHealth <= 0)
         {
@@ -35,6 +56,7 @@ public class playerHealth : MonoBehaviour
     {
         gameObject.SetActive(false);
         Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
+        damageScreen.color = flashColor;
         Destroy(gameObject, 0.3f);
     }
 }
